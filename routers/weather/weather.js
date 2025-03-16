91e05ccffbed0d3c9e1c04d95b86cc3239e8177a
hun <pursuit0819@naver.com>
2025-03-16 17:28:15 +0900

    express sample

--- routers/weather/weather.js
+++ routers/weather/weather.js
@@ -1,19 +1,30 @@
+import axios from 'axios';
 import { WEATHER_API_KEY, WEATHER_HOST_URL } from '../../config.js';
 
 export const weatherCallback = async (req, res) => {
   const { lat, lng } = req.body;
 
-  console.log(lat, lng);
-
   try {
-    const requset = await fetch(
-      `${WEATHER_HOST_URL}//forecast?lat=${lat}&lon=${lng}&appid=${WEATHER_API_KEY}`
+    const response = await axios.get(
+      `${WEATHER_HOST_URL}/forecast?lat=${lat}&lon=${lng}&appid=${WEATHER_API_KEY}&units=metric`
     );
 
-    const response = await requset.json();
-    console.log(response);
+    if (response.status === 200) {
+      const data = response.data;
 
-    return res.json({});
+      const convertKTCList = data.list.map((ls) => {
+        const date = new Date(ls.dt_txt);
+        date.setHours(date.getHours() + 9);
+
+        const targetDate = date.toISOString().split('.')[0];
+
+        const hour = targetDate.split('T')[1];
+        const today = targetDate.split('T')[0];
+
+        return { ...ls, dt_txt: `${today} ${hour}` };
+      });
+      return res.json({ list: convertKTCList });
+    }
   } catch (e) {
     console.error(e);
   }
